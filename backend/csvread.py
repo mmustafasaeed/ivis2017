f693d4e9772693a76e9d4b59e527ff1feccfa38d
Mohit Ahuja <mohit@houzify.com>
2017-02-22 14:01:26 +0100

    more

---
+++ backend/csvread.py
@@ -0,0 +1,42 @@
+import csv
+import json
+import sys
+
+print sys.argv
+print len(sys.argv)
+if (len(sys.argv) != 3):
+    print 'use format: python csvread.py <visualering_directory> <lopnr_new>'
+
+resp = {'pnr': sys.argv[2]}
+person_file = csv.DictReader(open(sys.argv[1] + '/person.csv'))
+for row in person_file:
+    if row['lopnr_new'] == sys.argv[2]:
+        print row
+        resp['person'] = row
+
+resp['biopsy'] = []
+biopsy_file = csv.DictReader(open(sys.argv[1] +  '/biopsy.csv'))
+for row in biopsy_file:
+    if row['lopnr_new'] == sys.argv[2]:
+        print row
+        resp['biopsy'].append(row)
+
+resp['diagnoses'] = []
+biopsy_file = csv.DictReader(open(sys.argv[1] +  '/diagnoses.csv'))
+for row in biopsy_file:
+    if row['lopnr_new'] == sys.argv[2]:
+        print row
+        resp['diagnoses'].append(row)
+
+resp['psa'] = []
+biopsy_file = csv.DictReader(open(sys.argv[1] +  '/psa.csv'))
+for row in biopsy_file:
+    if row['lopnr_new'] == sys.argv[2]:
+        print row
+        resp['psa'].append(row)
+
+
+with open(sys.argv[2] + '.json', 'w') as outfile:
+    json.dump(resp, outfile)
+
+
